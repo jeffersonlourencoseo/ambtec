4a20807263ead20bf5b92bf508f47bdfd75b1c93
Jefferson Lourenço <jefferson@exemplo.com>
2026-06-11 21:55:04 -0300

    fix(vercel): output static para eliminar erro de runtime nodejs18.x

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://licenciamentoambientalrio.com.br',
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   integrations: [tailwind(), mdx(), sitemap()],
 });
